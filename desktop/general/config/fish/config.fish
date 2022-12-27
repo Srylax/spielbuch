db58b1364013aaa2af3735ef1b2027274043d540
Kylian Lichtensteiger <Kylian.Lichtensteiger@bsi-software.com>
2022-12-27 17:02:53 +0100

    added fish and starship config

---
+++ desktop/general/config/fish/config.fish
@@ -0,0 +1,24 @@
+### ADDING TO THE PATH
+# First line removes the path; second line sets it.  Without the first line,
+# your path gets massive and fish becomes very slow.
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.local/share/gem/ruby/3.0.0/bin $HOME/.local/bin $HOME/Applications $fish_user_paths
+
+
+### EXPORT ###
+set fish_greeting
+
+export EDITOR='idea'
+export VISUAL='~/.local/bin/idea -e'
+#cat ~/.cache/wal/sequences
+
+### Alias
+#alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+alias fat='du * -shc'
+#alias smash='sudo python3 ~/dev/workspaces/fusee-launcher/fusee-launcher.py ~/dev/workspaces/fusee-launcher/payloads/hekate_ctcaer.bin'
+
+
+### Auto generated
+# thefuck --alias | source
+
+#source /opt/asdf-vm/asdf.fish
